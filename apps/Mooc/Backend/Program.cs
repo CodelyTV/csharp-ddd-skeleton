8aee0ac14d70291d8f3a76716d026e6196b5a34b
Leandro <leanwitzke@gmail.com>
2019-10-01 00:06:33 -0300

    Added mock backend app and health check endpoint

---
+++ apps/Mooc/Backend/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+        }
+    }
+}
