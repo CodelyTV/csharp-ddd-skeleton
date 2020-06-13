c4a10c6408f40f41cdce247c19a8cf66b9aa9acf
leanwit <leanwitzke@gmail.com>
2020-06-13 19:40:53 -0300

    [backoffice-frontend] Add courses counter element with real data

--- apps/Backoffice/Frontend/Controllers/Courses/CoursesGetWebController.cs
+++ apps/Backoffice/Frontend/Controllers/Courses/CoursesGetWebController.cs
@@ -1,15 +1,28 @@
-using Microsoft.AspNetCore.Mvc;
-
 namespace Frontend.Controllers.Courses
 {
+    using System.Threading.Tasks;
+    using CodelyTv.Mooc.CoursesCounter.Application.Find;
+    using CodelyTv.Shared.Domain.Bus.Query;
+    using Microsoft.AspNetCore.Mvc;
+
     [Route("Courses")]
     public class CoursesGetWebController : Controller
     {
-        public IActionResult Index()
+        private readonly IQueryBus _bus;
+
+        public CoursesGetWebController(IQueryBus bus)
         {
+            _bus = bus;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            CoursesCounterResponse counterResponse =
+                await _bus.Ask<CoursesCounterResponse>(new FindCoursesCounterQuery());
+
             ViewBag.Title = "Welcome";
             ViewBag.Description = "CodelyTV - Backoffice";
-            ViewBag.CoursesCounter = 5;
+            ViewBag.CoursesCounter = counterResponse.Total;
             return View("/Views/Courses/Index.cshtml");
         }
     }
--- apps/Backoffice/Frontend/Startup.cs
+++ apps/Backoffice/Frontend/Startup.cs
@@ -1,15 +1,19 @@
 namespace Frontend
 {
+    using CodelyTv.Apps.Mooc.Backend.Extension.DependencyInjection;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -19,6 +23,9 @@ namespace Frontend
         {
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddHealthChecks();
+
+            services.AddApplication();
+            services.AddInfrastructure(_configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
