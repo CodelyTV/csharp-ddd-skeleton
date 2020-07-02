061a10850ce9213a5d62a4c12e79f08f49774ea1
leanwit <leanwitzke@gmail.com>
2020-07-02 18:37:31 -0300

    Modify sql - create table if not exists

--- database/mooc.sql
+++ database/mooc.sql
@@ -1,18 +1,18 @@
-CREATE TABLE courses (
+CREATE TABLE IF NOT EXISTS courses (
   [id] CHAR(36) NOT NULL,
   [name] VARCHAR(255) NOT NULL,
   [duration] VARCHAR(255) NOT NULL,
   PRIMARY KEY ([id])
 )  ;
 
-CREATE TABLE courses_counter (
+CREATE TABLE IF NOT EXISTS courses_counter (
     [id] CHAR(36) NOT NULL,
     [total] INT NOT NULL,
     [existing_courses] nvarchar(max) NOT NULL,
     PRIMARY KEY ([id])
 )  ;
 
-CREATE TABLE domain_events (
+CREATE TABLE IF NOT EXISTS domain_events (
     [id] CHAR(36) NOT NULL,
     [aggregate_id] CHAR(36) NOT NULL,
     [name] VARCHAR(255) NOT NULL,
